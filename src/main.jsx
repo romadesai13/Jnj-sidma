130ac22f5a2b34832b76c935de9d2ef8d932c596
Roma Desai <romadesai13@gmail.com>
2023-10-10 16:15:49 -0700

    reaflow

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import AppFlow from './AppFlow'
 import './index.css'
 import "./styles.css";
+import Reaflow from './Reaflow'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppFlow />
+    {/* <AppFlow /> */}
+    <Reaflow />
   </React.StrictMode>,
 )
