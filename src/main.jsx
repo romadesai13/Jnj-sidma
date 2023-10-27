da41240192020f6bc1f180cbb226048b554181b6
Paritosh Fulsunge <paritosh.fulsunge@gmail.com>
2023-10-27 16:45:24 -0700

    adding vite config to host in github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppFlow from './AppFlow'
+import AppFlowELK from './AppFlowELK'
 import './index.css'
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppFlow />
+    <AppFlowELK />
   </React.StrictMode>,
 )
